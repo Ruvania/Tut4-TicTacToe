17fb525d8a742d80308b71fca1258b879d8393f7
Ruvania <214521716@gmail.com>
2016-03-16 12:16:53 +0200

    board created

---
+++ Tut4.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+using namespace std;
+
+class TicTacToe
+{
+public: int reset();
+		int board[3][3];
+		int player; //player1=1 player2=2
+		void DisplayBoard();
+		void NextPlayer();
+		int move(int,int);
+};
+
+int TicTacToe::reset()
+{
+	for (int i = 0; i < 3; i++)
+		for (int j = 0; j < 3; j++)
+			board[i][j] = 0;
+	player = 1; //player 1's turn 
+	DisplayBoard();
+}
+	
+void TicTacToe::DisplayBoard()
+{
+	int temp[9];
+	int k = 0;
+	for (int i = 0; i < 3; i++)
+		for (int j = 0; j < 3; j++)
+
+
+		{
+			if (board[i][j] == 0)
+				temp[k] = k + 49;
+			else
+
+
+			{
+				if (board[i][j] == 1)
+					temp[k] = 88;
+				else
+					temp[k] = 79;
+			}
+			k++;
+		}
+	cout << "+---+---+---+\n";
+	cout << "| " << (char)temp[0] << " | " << (char)temp[1] << " | " << (char)temp[2] << " | \n";
+	cout << "+---+---+---+\n";
+	cout << "| " << (char)temp[3] << " | " << (char)temp[4] << " | " << (char)temp[5] << " | \n";
+	cout << "+---+---+---+\n";
+	cout << "| " << (char)temp[6] << " | " << (char)temp[7] << " | " << (char)temp[8] << " | \n";
+	cout << "+---+---+---+\n";
+}
+	
+	void TicTacToe::NextPlayer()
+
+
+	{
+		if (player == 1)
+			player = 2;
+		else
+			player = 1;
+	}
+
+	int TicTacToe::move(int row, int col)
+	{
+      
+	}
+
+int main()
+{
+	TicTacToe obj;
+	obj.DisplayBoard();
+}
